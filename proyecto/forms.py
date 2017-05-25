c5d24b0f1ef02c0ab52cf081a730b535401d9e1d
Alejandro-Vega\ravp_ <ravp_92@hotmail.com>
2017-05-25 15:50:24 -0400

    ORDENADO

---
+++ proyecto/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+from proyecto.models import proyecto, Galeria
+
+
+class ProyectoForm(forms.ModelForm):
+#ogo = forms.ImageField()
+#galeria_imagenes = forms.ImageField()
+	class Meta:
+		model = proyecto
+
+		fields = [
+			'titulo',
+			'logo',
+			'descripcion_general',
+			'descripcion_detallada',
+			'definicion_problema',
+			'definicion_solucion',
+			'galeria_imagenes',
+			'video',
+		]
+		labels = {
+			'titulo': 'Nombre del Proyecto',
+		#	'logo' : 'Logo',
+			'descripcion_general': 'Descripcion General',
+			'descripcion_detallada': 'Visión general del negocio',
+			'definicion_problema':'Definicion del Problema',
+			'definicion_solucion': 'Solucion propuesta',
+		#   'galeria_imagenes' : 'Galeria de Imagenes',
+		}
+		widgets = {
+			'titulo': forms.TextInput(attrs={'class':'form-control'}),
+			'descripcion_detallada': forms.Textarea(attrs={'class':'form-control'}),
+			'descripcion_general': forms.Textarea(attrs={'class':'form-control'}),
+			'definicion_problema': forms.Textarea(attrs={'class':'form-control'}),
+			'definicion_solucion': forms.Textarea(attrs={'class':'form-control'}),
+		}
--- src/proyecto/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from proyecto.models import proyecto, Galeria, Comment
-# Register your models here.
-
-admin.site.register(proyecto)
-admin.site.register(Comment)
-admin.site.register(Galeria)
